7a600d0860894f8b9e50defdac823ca799334be7
kteraguchi <kteraguchi@commonsnet.org>
2014-10-14 14:28:57 +0900

    Defined router

--- Config/routes.php
+++ Config/routes.php
@@ -10,21 +10,50 @@
 
 App::uses('Page', 'Pages.Model');
 
-Router::connect('/' . Page::SETTING_MODE_WORD . '/:plugin/:controller/:action/*',
-	array());
+$defaults = array('action' => 'index');
+$pluginOptions = array();
+$shortOptions = array();
+$controllerOptions = array();
 
-Router::connect('/' . Page::SETTING_MODE_WORD . '/*',
-	array(
-		'plugin' => 'pages',
-		'controller' => 'pages',
-		'action' => 'index'));
+if ($plugins = CakePlugin::loaded()) {
+	App::uses('PluginShortRoute', 'Routing/Route');
+	foreach ($plugins as $key => $value) {
+		$plugins[$key] = Inflector::underscore($value);
+	}
+	$pattern = implode('|', $plugins);
+	$pluginOptions = array('plugin' => $pattern);
+	$shortOptions = array('routeClass' => 'PluginShortRoute', 'plugin' => $pattern);
+}
 
-Router::connect('/:plugin/:controller/:action/*',
-	array());
+if ($controllers = App::objects('controller')) {
+	foreach ($controllers as $key => $value) {
+		$pos = strrpos($value, 'Controller');
+		$controllers[$key] = Inflector::underscore(substr($value, 0, $pos));
+	}
+	$pattern = implode('|', $controllers);
+	$controllerOptions = array('controller' => $pattern);
+}
 
+Router::connect('/' . Page::SETTING_MODE_WORD . '/:plugin', $defaults, $shortOptions);
+Router::connect('/' . Page::SETTING_MODE_WORD . '/:plugin/:controller', $defaults, $pluginOptions);
+Router::connect('/' . Page::SETTING_MODE_WORD . '/:plugin/:controller/:action/*', array(), $pluginOptions);
 
-Router::connect('/*',
-	array(
-		'plugin' => 'pages',
-		'controller' => 'pages',
-		'action' => 'index'));
+Router::connect('/' . Page::SETTING_MODE_WORD . '/:controller', $defaults, $controllerOptions);
+Router::connect('/' . Page::SETTING_MODE_WORD . '/:controller/:action/*', array(), $controllerOptions);
+
+$pageDefaults = array(
+	'plugin' => 'pages',
+	'controller' => 'pages',
+	'action' => 'index');
+Router::connect('/' . Page::SETTING_MODE_WORD . '/*', $pageDefaults);
+
+Router::connect('/:plugin', $defaults, $shortOptions);
+Router::connect('/:plugin/:controller', $defaults, $pluginOptions);
+Router::connect('/:plugin/:controller/:action/*', array(), $pluginOptions);
+
+Router::connect('/:controller', array('action' => 'index'), $controllerOptions);
+Router::connect('/:controller/:action/*', array(), $controllerOptions);
+
+Router::connect('/*', $pageDefaults);
+
+unset($defaults, $pluginOptions, $shortOptions, $controllerOptions, $pattern, $pos, $pageDefaults);
