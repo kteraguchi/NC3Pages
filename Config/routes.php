f8798fb480548f468f423a1540bb7bd8143d8175
kteraguchi <kteraguchi@commonsnet.org>
2014-10-24 16:10:12 +0900

    Use slug route

--- Config/routes.php
+++ Config/routes.php
@@ -9,51 +9,8 @@
  */
 
 App::uses('Page', 'Pages.Model');
+App::uses('SlugRoute', 'Pages.Lib');
 
-$defaults = array('action' => 'index');
-$pluginOptions = array();
-$shortOptions = array();
-$controllerOptions = array();
-
-if ($plugins = CakePlugin::loaded()) {
-	App::uses('PluginShortRoute', 'Routing/Route');
-	foreach ($plugins as $key => $value) {
-		$plugins[$key] = Inflector::underscore($value);
-	}
-	$pattern = implode('|', $plugins);
-	$pluginOptions = array('plugin' => $pattern);
-	$shortOptions = array('routeClass' => 'PluginShortRoute', 'plugin' => $pattern);
-}
-
-if ($controllers = App::objects('controller')) {
-	foreach ($controllers as $key => $value) {
-		$pos = strrpos($value, 'Controller');
-		$controllers[$key] = Inflector::underscore(substr($value, 0, $pos));
-	}
-	$pattern = implode('|', $controllers);
-	$controllerOptions = array('controller' => $pattern);
-}
-
-Router::connect('/' . Page::SETTING_MODE_WORD . '/:plugin', $defaults, $shortOptions);
-Router::connect('/' . Page::SETTING_MODE_WORD . '/:plugin/:controller', $defaults, $pluginOptions);
-Router::connect('/' . Page::SETTING_MODE_WORD . '/:plugin/:controller/:action/*', array(), $pluginOptions);
-
-Router::connect('/' . Page::SETTING_MODE_WORD . '/:controller', $defaults, $controllerOptions);
-Router::connect('/' . Page::SETTING_MODE_WORD . '/:controller/:action/*', array(), $controllerOptions);
-
-$pageDefaults = array(
-	'plugin' => 'pages',
-	'controller' => 'pages',
-	'action' => 'index');
-Router::connect('/' . Page::SETTING_MODE_WORD . '/*', $pageDefaults);
-
-Router::connect('/:plugin', $defaults, $shortOptions);
-Router::connect('/:plugin/:controller', $defaults, $pluginOptions);
-Router::connect('/:plugin/:controller/:action/*', array(), $pluginOptions);
-
-Router::connect('/:controller', array('action' => 'index'), $controllerOptions);
-Router::connect('/:controller/:action/*', array(), $controllerOptions);
-
-Router::connect('/*', $pageDefaults);
-
-unset($defaults, $pluginOptions, $shortOptions, $controllerOptions, $pattern, $pos, $pageDefaults);
+Router::connect('/' . Page::SETTING_MODE_WORD . '/', array('plugin' => 'pages', 'controller' => 'pages', 'action' => 'index'), array('routeClass' => 'SlugRoute'));
+Router::connect('/' . Page::SETTING_MODE_WORD . '/*', array('plugin' => 'pages', 'controller' => 'pages', 'action' => 'index'), array('routeClass' => 'SlugRoute'));
+Router::connect('/*', array('plugin' => 'pages', 'controller' => 'pages', 'action' => 'index'), array('routeClass' => 'SlugRoute'));
---
+++ Lib/SlugRoute.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * SlugRoute
+ *
+ * Automatically slugs routes based on named parameters
+ *
+ */
+class SlugRoute  extends CakeRoute {
+
+/**
+ * parse
+ *
+ * @param string $url The URL to attempt to parse.
+ * @return mixed Boolean false on failure, otherwise an array or parameters
+ */
+	function parse($url) {
+		$params = parent::parse($url);
+
+		if (empty($params)) {
+			return false;
+		}
+
+		$path = implode('/', $params['pass']);
+		$Page = new Page();
+		$count = $Page->find('count', array(
+			'conditions' => array('Page.permalink' => $path),
+			'recursive' => -1
+		));
+
+		if ($count) {
+			return $params;
+		}
+
+		return false;
+	}
+
+}
+
+?>
