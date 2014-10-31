2f11dc8f614196f0b486a8f687fc5dc36d991e9c
kteraguchi <kteraguchi@commonsnet.org>
2014-10-31 19:41:07 +0900

    It feels good

--- Config/routes.php
+++ Config/routes.php
@@ -14,3 +14,25 @@ App::uses('SlugRoute', 'Pages.Lib');
 Router::connect('/' . Page::SETTING_MODE_WORD . '/', array('plugin' => 'pages', 'controller' => 'pages', 'action' => 'index'), array('routeClass' => 'SlugRoute'));
 Router::connect('/' . Page::SETTING_MODE_WORD . '/*', array('plugin' => 'pages', 'controller' => 'pages', 'action' => 'index'), array('routeClass' => 'SlugRoute'));
 Router::connect('/*', array('plugin' => 'pages', 'controller' => 'pages', 'action' => 'index'), array('routeClass' => 'SlugRoute'));
+
+$params = array(Page::SETTING_MODE_WORD => false);
+if (Page::isSetting()) {
+	$params = array();
+}
+$indexParams = $params + array('action' => 'index');
+if ($plugins = CakePlugin::loaded()) {
+	App::uses('PluginShortRoute', 'Routing/Route');
+	foreach ($plugins as $key => $value) {
+		$plugins[$key] = Inflector::underscore($value);
+	}
+	$pluginPattern = implode('|', $plugins);
+	$match = array('plugin' => $pluginPattern);
+	$shortParams = array('routeClass' => 'PluginShortRoute', 'plugin' => $pluginPattern);
+
+	Router::connect('/' . Page::SETTING_MODE_WORD  . '/:plugin', $indexParams, $shortParams);
+	Router::connect('/' . Page::SETTING_MODE_WORD  . '/:plugin/:controller', $indexParams, $match);
+	Router::connect('/' . Page::SETTING_MODE_WORD  . '/:plugin/:controller/:action/*', $params, $match);
+}
+
+Router::connect('/' . Page::SETTING_MODE_WORD  . '/:controller', $indexParams);
+Router::connect('/' . Page::SETTING_MODE_WORD  . '/:controller/:action/*',  $params);
